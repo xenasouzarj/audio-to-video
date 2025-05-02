12b2151465dff915fcb0ba0afbb5638cb759b019
xenasouzarj <42729848+xenasouzarj@users.noreply.github.com>
2025-05-02 04:09:55 -0300

    Add files via upload

---
+++ Qwen_bash_20250502_j942wptse.sh
@@ -0,0 +1,14 @@
+# Inicialize o repositório local
+git init
+
+# Adicione os arquivos
+git add .
+
+# Commit inicial
+git commit -m "Primeiro commit - Projeto completo de conversão de áudio para vídeo"
+
+# Conecte ao repositório remoto (substitua <seu-usuario> pelo seu nome no GitHub)
+git remote add origin https://github.com/<seu-usuario>/audio-to-video.git
+
+# Envie os arquivos
+git push -u origin master
